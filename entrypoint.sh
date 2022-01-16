f27ee120cb4e096aae9dc544667ffeac52090290
comame <dev@comame.xyz>
2022-01-16 22:15:25 +0900

    broken

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ FILENAME=$1
 export PATH=$PATH:$(dirname $(find /usr/local/texlive/ -name "tlmgr"))
 
 cp -R ~/* /mystyle.sty /tmp/
+
 cd /tmp
 ptex2pdf -u -l $FILENAME > /dev/null
 ptex2pdf -u -l $FILENAME | tail
-cp /tmp/$FILENAME.pdf /tmp/$FILENAME.log ~/
+
+rm -f ~/$FILENAME.pdf ~/$FILENAME.log
+cp -f /tmp/$FILENAME.pdf /tmp/$FILENAME.log ~/
